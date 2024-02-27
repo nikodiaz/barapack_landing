9337b247fd345ef1fc6a40eda37aa0112bc1fbc3
Nicolás Díaz <nikodiaz56@gmail.com>
2024-02-27 11:06:22 -0300

    fix: 🛠️ desktop footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,8 +7,8 @@ import Logo from '../NavBar/Logo';
 
 const Footer = () => {
 	return (
-		<footer className='grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 place-items-center gap-y-8 py-8 min-h-40 text-white bg-[#3E4453]'>
-			<div className='flex flex-col-reverse items-center md:flex-row gap-8'>
+		<footer className='grid grid-rows-2 md:grid-rows-1 md:grid-cols-3 place-items-center gap-y-8 py-8 min-h-40 text-white bg-[#3E4453]'>
+			<div className='flex flex-col-reverse items-center md:justify-evenly md:flex-row gap-8 md:col-start-1 md:col-end-3 w-full'>
 				<div className='flex items-center gap-4'>
 					All rights reserved by{' '}
 					<Logo logo={logo} styles='hidden md:inline-block w-32 h-32' />
@@ -21,7 +21,7 @@ const Footer = () => {
 					<Logo logo={whatsapp} styles='w-12 h-12' />
 				</div>
 			</div>
-			<div className='flex items-center gap-4'>
+			<div className='flex items-center gap-4 md:col-start-3 md:col-end-4'>
 				Developed by <Logo logo={futurisi} styles='w-20 h-20' />
 			</div>
 		</footer>
